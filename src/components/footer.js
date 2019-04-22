706e6e022c5bc98ce0a6235547762a35b376c5ba
Bohdan Galt <39914884+namegalt@users.noreply.github.com>
2019-04-22 23:42:49 +0300

    fix contacts

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,9 +12,9 @@ const Footer = () => (
         <a href="mailto:aaleksandravovk@gmail.com">aaleksandravovk@gmail.com</a>
         <a name="contacts" style={{visibility: "hidden"}}/>
     <SocialWrapper>
-        <a href="https:www.facebook.com/profile.php?id=100006372695194" target="_blank"><img src={facebook}></img></a>
-        <a href="https:www.instagram.com/san_dora_" target="_blank"><img src={instagram}></img></a>
-        <a href="https:www.t.me/sandravovk" target="_blank"><img src={telegram}></img></a>
+        <a href="https://www.facebook.com/profile.php?id=100006372695194" target="_blank"><img src={facebook}></img></a>
+        <a href="https://www.instagram.com/san_dora_" target="_blank"><img src={instagram}></img></a>
+        <a href="https://www.t.me/sandravovk" target="_blank"><img src={telegram}></img></a>
         {/* <a href="mailto:namegalt@email.com" target="_blank"><img src={mail}></img></a> */}
     </SocialWrapper>
     </FooterWrapper>
